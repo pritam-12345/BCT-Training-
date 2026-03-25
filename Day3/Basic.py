3c4941099cb021fce978cf4b78c3f8b7eadfc334
Pritam Saha <pritam7230saha@gmail.com>
2026-03-25 15:43:03 +0530

    Initial commit V0 Day1,Day2"

---
+++ Day3/Basic.py
@@ -0,0 +1,30 @@
+print("hello Students")
+print("""This is
+written using
+python""")
+a=10
+print(a)
+print(type(a))
+<class 'int'>
+print (type(b))
+<class 'float'>
+s='Hello World'
+# Data Types:
+# Numbers:int,float,complex,long
+# String:stwe can make it in '' or " "
+# List: []
+# Tuple: ()
+# Dictionary: {}
+# Set: {}
+#Operator:
+#Arithmetic Operator: +,-,*,/,%,//,**
+print (2*3)
+print (6/2)
+#Write a python program for swapping of two numbers withoiut using temp or third variable
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a=",a)
+print("b=",b)
